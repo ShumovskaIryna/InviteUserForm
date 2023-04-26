e8f52c471c4a22a26535fc103f55d994591ff5fc
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-04-26 18:33:21 +0300

    add footer with button

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="item">
+    <button>Next Step</button>
+  </div>
+</template>
+
+<style scoped>
+button{
+  background-color: #1D2452;
+  border: none;
+  border-radius: 20px;
+  padding: 5px 10px;
+  color: white;
+  margin: 20px;
+}
+</style>
